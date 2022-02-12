eb519a73963a606d2c12f65343abf9afa577c39a
Mariusz Staniak <mariusz.staniak@gmail.com>
2022-02-12 09:44:11 +0100

    init commit

---
+++ src/main/kotlin/com/example/elastic_playgroud/ElasticPlaygroudApplication.kt
@@ -0,0 +1,11 @@
+package com.example.elastic_playgroud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ElasticPlaygroudApplication
+
+fun main(args: Array<String>) {
+    runApplication<ElasticPlaygroudApplication>(*args)
+}
